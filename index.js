6400bed844eee44acbc3aa08b5e3cd59f25cdf16
ethcala <81577564+ethcala@users.noreply.github.com>
2023-04-06 08:38:42 -0600

    Outlined routes

--- index.js
+++ index.js
@@ -11,4 +11,32 @@ app.use((req,res,next) => {
 
 app.get('/api',routes.api)
 
+app.post('/register', routes.register)
+
+app.post('/login', routes.login)
+
+app.put('/user', routes.updateUser)
+
+app.get('/user', routes.getUser)
+
+app.delete('/user', routes.deleteUser)
+
+app.post('/message', routes.createMessage)
+
+app.get('/messages', routes.getMessagesByUser)
+
+app.get('/order', routes.getOrder)
+
+app.post('/order', routes.createOrder)
+
+app.get('/ordersByShop', routes.getOrdersByShop)
+
+app.get('/ordersByUser', routes.getOrdersByUser)
+
+app.put('/order', routes.updateOrder)
+
+app.post('/wishlist', routes.addToWishlist)
+
+app.delete('/wishlist', routes.deleteFromWishlist)
+
 app.listen(3000);
