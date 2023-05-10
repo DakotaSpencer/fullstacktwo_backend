8996316e2119a1a09e62e05813ea5f52aacb2a6b
Riley Shipman <108430567+Cal-V@users.noreply.github.com>
2023-05-10 08:38:10 -0600

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const routes = require("./routes/routes")
 const express = require("express")
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
